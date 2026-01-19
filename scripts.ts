414267b2fe08bbd724f922b582d217ed5b54c189
leonardoqg <leonardoqgaldino@gmail.com>
2026-01-19 10:24:14 -0300

    commit 1

---
+++ scripts.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+// Exemplos de uso
+console.log(multiplicar(5, 3)); // Saída: 15
+console.log(saudacao("Maria")); // Saída: Olá Maria
